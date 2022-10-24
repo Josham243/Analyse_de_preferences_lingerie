710063a4e6fdb50b49eec46d372a9cbcaf9a719e
Josham243 <jonathankabiebwe98@gmail.com>
2022-10-24 03:28:44 +0100

    envoi de mes fichiers existants

--- Prediction/streamlit.py
+++
@@ -1,8 +0,0 @@
-import pickle 
-import streamlit as st
- 
-model = pickle.load(open('C:/Users/Josham Kadiebwe/Desktop/python/Datasets/Dataset2/Jonathan Equilibrage de donnee/Amazon_rfc.pkl', 'rb'))
- 
-
-def main ():
-     st.title('Lingerie fabric')
